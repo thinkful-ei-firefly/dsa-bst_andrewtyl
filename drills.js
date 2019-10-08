c758ebd8aabf18185e2a9896a86a72401c1377cc
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-08 11:33:39 -0500

    5

--- drills.js
+++ drills.js
@@ -30,4 +30,41 @@ function main3() {
     console.log(bst2)
 }
 
-main3()
+function main4() {
+
+    function findHeightOf(bst) {
+        let height = 0
+        if (bst !== null) {
+            height++
+            lheight = findHeightOf(bst.left)
+            rheight = findHeightOf(bst.right)
+            if (lheight > rheight) {
+                height = height + lheight
+            }
+            else if (rheight > lheight) {
+                height = height + rheight
+            }
+            else if (lheight === rheight) {
+                height = height + lheight
+            }
+            else {
+                throw new Error('This line of code should never trigger. You broke something.')
+            }
+        }
+        return height;
+    }
+
+    const bst1 = new BST
+    bst1.insert(3, true)
+    bst1.insert(1, true)
+    bst1.insert(4, true)
+    bst1.insert(6, true)
+    bst1.insert(9, true)
+    bst1.insert(2, true)
+    bst1.insert(5, true)
+    bst1.insert(7, true)
+    console.log(findHeightOf(bst1))
+
+}
+
+main4()
